61671e335c01102eaf8362c19a7b409b299066e4
zen <zen.dev@yandex.com>
2015-01-28 12:56:06 +0200

    init

---
+++ Persei/Persei.h
@@ -0,0 +1,19 @@
+//
+//  Persei.h
+//  Persei
+//
+//  Created by zen on 28/01/15.
+//  Copyright (c) 2015 Yalantis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Persei.
+FOUNDATION_EXPORT double PerseiVersionNumber;
+
+//! Project version string for Persei.
+FOUNDATION_EXPORT const unsigned char PerseiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Persei/PublicHeader.h>
+
+
