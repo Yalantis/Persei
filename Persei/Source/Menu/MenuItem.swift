7eec846019650ff2cc76b07f485e3d4328a6db13
zen <zen.dev@yandex.com>
2015-01-31 18:51:15 +0200

    Update default values for Menu Item

--- Persei/Source/Menu/MenuItem.swift
+++ Persei/Source/Menu/MenuItem.swift
@@ -12,6 +12,8 @@ public struct MenuItem {
     public var image: UIImage
     public var highlightedImage: UIImage?
     
-    public var backgroundColor = UIColor.blackColor()
-    public var highlightedBackgroundColor = UIColor.redColor()
+    public var backgroundColor = UIColor(red: 50.0 / 255.0, green: 49.0 / 255.0, blue: 73.0 / 255.0, alpha: 1.0)
+    public var highlightedBackgroundColor = UIColor(red: 1.0, green: 61.0 / 255.0, blue: 130.0 / 255.0, alpha: 1.0)
+    
+    public var shadowColor = UIColor(red: 41.0 / 255.0, green: 44.0 / 255.0, blue: 69.0 / 255.0, alpha: 1.0)
 }
