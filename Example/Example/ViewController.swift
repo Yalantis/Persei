5b6c7082519deba7f30dbdd58be3bf3bcc4cefee
zen <zen.dev@yandex.com>
2015-02-02 12:26:31 +0200

    Add background & navigation bar icons

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
         menu.backgroundColor = UIColor(red: 51.0 / 255.0, green: 51.0 / 255.0, blue: 75.0 / 255.0, alpha: 1.0)
         
         menu.items = (0..<7 as Range).map {
-            MenuItem(image: UIImage(named: "icon_\($0)")!)
+            MenuItem(image: UIImage(named: "menu_icon_\($0)")!)
         }
     
         tableView.addSubview(menu)
