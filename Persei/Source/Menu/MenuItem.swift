43245abd29f4c8421e3214bb27b43a7ff208449d
zen <zen.dev@yandex.com>
2015-03-04 14:54:31 +0200

    Update default shadow color for menu cells

--- Persei/Source/Menu/MenuItem.swift
+++ Persei/Source/Menu/MenuItem.swift
@@ -13,7 +13,7 @@ public struct MenuItem {
     public var backgroundColor = UIColor(red: 50.0 / 255.0, green: 49.0 / 255.0, blue: 73.0 / 255.0, alpha: 1.0)
     public var highlightedBackgroundColor = UIColor(red: 1.0, green: 61.0 / 255.0, blue: 130.0 / 255.0, alpha: 1.0)
     
-    public var shadowColor = UIColor(white: 0.3, alpha: 0.3)
+    public var shadowColor = UIColor(white: 0.1, alpha: 0.3)
     
     // MARK: - Init
     public init(image: UIImage, highlightedImage: UIImage? = nil) {
