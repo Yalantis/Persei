a3844fd29f8d28f67467baf7f25b0fdf91cb5229
zen <zen.dev@yandex.com>
2015-03-06 12:29:52 +0200

    Pod add UIImage+Autoresize

---
+++ Example/Pods/Target Support Files/Pods-Example-UIImage+Autoresize/Pods-Example-UIImage+Autoresize-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "UIImage+Autoresize.h"
+
+FOUNDATION_EXPORT double UIImage_AutoresizeVersionNumber;
+FOUNDATION_EXPORT const unsigned char UIImage_AutoresizeVersionString[];
+
